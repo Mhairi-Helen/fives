35487bab6b485f603661e840ad8dcd71d8089c6a
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-06-28 23:21:56 +0100

    Updates

---
+++ precache-manifest.f5f32f3962eb90f678c769fedb9abcfe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d9e9aac81094ff93656d2e2dc1f5f5",
+    "url": "/fives/index.html"
+  },
+  {
+    "revision": "435cf813f3251470ebba",
+    "url": "/fives/static/css/main.fa4823dc.chunk.css"
+  },
+  {
+    "revision": "a6129a2d86eafc4c887c",
+    "url": "/fives/static/js/2.80a1e177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fives/static/js/2.80a1e177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435cf813f3251470ebba",
+    "url": "/fives/static/js/main.186aeadd.chunk.js"
+  },
+  {
+    "revision": "ee6a54746b8120a41a2b",
+    "url": "/fives/static/js/runtime-main.42d03706.js"
+  },
+  {
+    "revision": "9bc50174bd29698659b7cb13417cd2dd",
+    "url": "/fives/static/media/football2.9bc50174.png"
+  },
+  {
+    "revision": "481ccf56e2899931e3c0af93f037b3b2",
+    "url": "/fives/static/media/football3.481ccf56.png"
+  },
+  {
+    "revision": "db6c273942329144b19b37294b8a5e34",
+    "url": "/fives/static/media/strip1.db6c2739.png"
+  },
+  {
+    "revision": "3272eb46ba1feaa705a64c059119fbe8",
+    "url": "/fives/static/media/strip2.3272eb46.png"
+  }
+]);
