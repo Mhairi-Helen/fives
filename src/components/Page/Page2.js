5d9d57bc696c50e38b297911db1c12e758811f93
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-06-30 18:19:14 +0100

    adding media queries to header

--- src/components/Page/Page2.js
+++ src/components/Page/Page2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
 import Team1 from '../TeamList/Team1';
 import Team2 from '../TeamList/Team2';
@@ -7,6 +7,7 @@ import TeamNames from '../TeamNames';
 const Page2 = () => {
 
     return (
+
         < div className="page--two">
             <header className="page__header--two">
                 <h4>Step 2: Choose Team Name</h4>
@@ -15,10 +16,11 @@ const Page2 = () => {
             <Team1 />
             <Team2 />
             <button
-                className={"button--primary"}>
+                className={"button--primary grid__group--right"}>
                 <Link to="/step3">On to Step 3</Link>
             </button>
         </div >
+
     )
 }
 
